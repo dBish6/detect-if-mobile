8f9bcf7f82a6582f230964b9593219fea2bdace6
David <davidbish2002@hotmail.com>
2024-02-29 01:36:29 -0330

    v1.17; Removed checking for touch support because it was causing issues with production level applications and not only mobile devices can have touch support.

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 export const isMobile = () => {
   const mobileKeywords =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      ),
-    touchSupport = "ontouchstart" in window || navigator.maxTouchPoints > 0;
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
 
-  return mobileKeywords || touchSupport;
+  return mobileKeywords;
 };
